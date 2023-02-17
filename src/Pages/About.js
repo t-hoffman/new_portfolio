58ed39d49acd4f4a73ec0d0fd85707db7331e1cc
Tyler Hoffman <tjhoffmn@gmail.com>
2023-02-16 22:18:01 -0800

    added links on about me

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,6 +10,18 @@ const About = () => {
       <p>I am an extremely motivated software engineer, proud member of the LGBT+ 🌈 community, with a serious passion for technology discovered at an early age. Self-taught and independent learner, I achieved success as a freelance full-stack developer at the age of 12 through self-motivated learning and online resources becoming skilled in both back-end and front-end design using PHP/MySQL, HTML/CSS, JavaScript, AJAX/XML and Adobe Photoshop.</p>
       <p>What excites me most about development is when I am able to help others find solutions to seemingly complex issues as well as creating stunning experiences that amaze its users and inspire fellow developers.  Proven ability in object-oriented programming, graphic design, and use of popular frameworks and libraries.  I am a dedicated, independent learner and problem solver, seeking to jumpstart my career in software engineering by collaborating with a dynamic team to drive exceptional results and growth in a fast-paced industry.</p>
       <p>Thank you 🙌 for taking the time to visit my portfolio I truly hope we are able to connect soon.  In a nutshell I'd say I am a genuine, humble and charismatic person that will add value to your team with my experience, skill set, ideas and relentless determination to find leading solutions.</p>
+      <p style={{marginTop:'50px'}}>
+        <a href="/Resume.pdf" target="_blank" rel="noreferrer" style={{fontFamily:'Montserrat', fontSize:'18pt'}}>RESUME</a>
+      </p>
+      <p>
+        <a href="/projects" style={{fontFamily:'Montserrat', fontSize:'18pt'}}>PROJECTS</a>
+      </p>
+      <p>
+        <a href="/skills" style={{fontFamily:'Montserrat', fontSize:'18pt'}}>SKILLS</a>
+      </p>
+      <p>
+        <a href="/contactme" style={{fontFamily:'Montserrat', fontSize:'18pt'}}>CONTACT ME</a>
+      </p>
     </div>
   )
 }
