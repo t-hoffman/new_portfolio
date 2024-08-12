42c005612885af2dfbd93baebdaf464058b990b5
t-hoffman <tjhoffmn@gmail.com>
2024-08-11 17:31:15 -0700

    Removed Navigate - unused var

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from 'Layout/Layout'
 import Home from 'Pages/Home'
 import Projects from 'Pages/Projects'
