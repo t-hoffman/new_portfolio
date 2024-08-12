246828c7532358fc48766ea1de8ffdab9b2ec022
t-hoffman <tjhoffmn@gmail.com>
2024-08-11 17:16:01 -0700

    Remove * redirect route

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ export default function App() {
       <Route path="/skills" element={<Skills />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/contactme" element={<Contact />} />
-      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
    </Layout> 
   )
